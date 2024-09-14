508806c9fe93a52c8a5fb91a3174086fdabecce0
kaljitism <dwikaljit@gmail.com>
2024-09-14 20:22:07 +0530

    testing lexer

--- lexer.ts
+++ lexer.ts
@@ -121,8 +121,7 @@ export function tokenize(sourceCode: string): Token[] {
   return tokens;
 }
 
-
-const source = await Deno.readTextFile("./lexer.ts");
+const source = await Deno.readTextFile("./src.code");
 for (const token of tokenize(source)) {
   console.log(token)
 }
